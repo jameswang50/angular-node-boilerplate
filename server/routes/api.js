6800bb727c69abff7fc45734e890f8dd2567329f
SuperPro1226 <javonwoods252@gmail.com>
2024-05-10 13:54:07 +0200

    updated server side

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('Checking For NODE paths');
 });
+
 
 module.exports = router;
