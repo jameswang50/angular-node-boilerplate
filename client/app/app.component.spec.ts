4122e69b261e38dcd79cf0c6527a14cb1188470c
SuperPro1226 <javonwoods252@gmail.com>
2024-05-15 16:14:00 +0200

    updated client

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -19,6 +19,8 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
+
+
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
